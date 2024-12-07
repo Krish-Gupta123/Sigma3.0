f0647bf052ff284bc2100c59ac056672ab1597ab
Krish-Gupta123 <krishrgupta5@gmail.com>
2024-12-07 14:09:02 +0530

    daily progress nothing special

---
+++ DSA/DSA/1.)Problem Solving Techniques/2.Bit Manipulation/bitManipulation.java
@@ -0,0 +1,75 @@
+// //BITWISE OPERATORS: AND,OR,XOR,1'S COMPLEMENT,LEFT SHIFT,RIGHT SHIFT
+
+// import java.util.*;
+
+// public class bitManipulation {
+// public static void main(String[] args) {
+// System.out.println("BitWise operators : ");
+// System.out.println("AND : " + (5 & 6));
+// System.out.println("OR : " + (5 | 6));
+// System.out.println("XOR : " + (5 ^ 6));
+// System.out.println("BINARY 1's COMPLEMENT : " + (~5) + " , " + (~0));
+// System.out.println("BINARY LEFT SHIFT : " + (5 << 2));
+// System.out.println("BINARY RIGHT SHIFT : " + (6 >> 1));
+// }
+// }
+
+// check if odd or even
+
+// import java.util.*;
+
+// public class bitManipulation {
+
+// public static void oddOrEven(int n) {
+// int bitmask = 1;
+// if ((n & bitmask) == 0) {
+// // even case
+// System.out.println("Even");
+// } else {
+// // odd case
+// System.out.println("Odd");
+// }
+// }
+
+// public static void main(String[] args) {
+// oddOrEven(1);
+// oddOrEven(4);
+// oddOrEven(8);
+// oddOrEven(999);
+// }
+// }
+
+// operations :
+// 1.)Get ith bit
+// 2.)Set th bit
+// 3.)Clear ith bit
+
+// 1.)Get ith bit
+
+// import java.util.*;
+
+// public class bitManipulation{
+// public static void main(String[] args) {
+
+// }
+// }
+
+// 2.)Set th bit
+
+// import java.util.*;
+
+// public class bitManipulation{
+// public static void main(String[] args) {
+
+// }
+// }
+
+// 3.)Clear ith bit
+
+// import java.util.*;
+
+// public class bitManipulation{
+// public static void main(String[] args) {
+
+// }
+// }
