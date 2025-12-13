b234996d737954ca7ab0c343a6bf643233ee9be8
Krish-Gupta123 <krishrgupta5@gmail.com>
2025-12-14 02:54:36 +0530

    dsa queues completed all

--- DSA/DSA/3.)Linear data structures/3.)Stacks and Queues/Queues/Queues.java
+++ DSA/DSA/3.)Linear data structures/3.)Stacks and Queues/Queues/Queues.java
@@ -75,94 +75,695 @@
 // }
 
 // // *****************************circular queue implementation using array
-public class Queues {
+// public class Queues {
 
-    static class Queue {
-        static int[] arr;
-        static int size;
-        static int front;
-        static int rear;
+// static class Queue {
+// static int[] arr;
+// static int size;
+// static int front;
+// static int rear;
 
-        Queue(int n) {
-            arr = new int[n];
-            size = n;
-            front = -1;
-            rear = -1;
-        }
+// Queue(int n) {
+// arr = new int[n];
+// size = n;
+// front = -1;
+// rear = -1;
+// }
 
-        public static boolean isEmpty() {
-            return front == -1 && rear == -1;
-        }
+// public static boolean isEmpty() {
+// return front == -1 && rear == -1;
+// }
 
-        public static boolean isFull() {
-            return (rear + 1) % size == front;
-        }
+// public static boolean isFull() {
+// return (rear + 1) % size == front;
+// }
 
-        // add : O(1)
-        public static void add(int num) {
-            if (isFull()) {
-                System.out.println("Queue is full");
-                return;
-            }
+// // add : O(1)
+// public static void add(int num) {
+// if (isFull()) {
+// System.out.println("Queue is full");
+// return;
+// }
 
-            if (isEmpty()) {
-                rear = front = 0;
-                arr[rear] = num;
-            } else {
-                rear = (rear + 1) % size;
-                arr[rear] = num;
-            }
+// if (isEmpty()) {
+// rear = front = 0;
+// arr[rear] = num;
+// } else {
+// rear = (rear + 1) % size;
+// arr[rear] = num;
+// }
 
-        }
+// }
 
-        // remove : O(1)
-        public static int remove() {
-            if (isEmpty()) {
-                System.out.println("Queue is empty");
-                return -1;
-            }
+// // remove : O(1)
+// public static int remove() {
+// if (isEmpty()) {
+// System.out.println("Queue is empty");
+// return -1;
+// }
 
-            int el = arr[front];
+// int el = arr[front];
 
-            // for last element
-            if (front == rear) {
-                front = rear = -1;
-            } else {
-                front = (front + 1) % size;
-            }
+// // for last element
+// if (front == rear) {
+// front = rear = -1;
+// } else {
+// front = (front + 1) % size;
+// }
 
-            return el;
+// return el;
 
-        }
+// }
 
-        // peek:O(1)
-        public static int peek() {
-            if (isEmpty()) {
-                System.out.println("Queue is empty");
-                return -1;
-            }
+// // peek:O(1)
+// public static int peek() {
+// if (isEmpty()) {
+// System.out.println("Queue is empty");
+// return -1;
+// }
 
-            return arr[front];
-        }
+// return arr[front];
+// }
 
-    }
+// }
 
-    public static void main(String[] args) {
-        Queue q = new Queue(3);
+// public static void main(String[] args) {
+// Queue q = new Queue(3);
 
-        q.add(1);
-        q.add(2);
-        q.add(3);
+// q.add(1);
+// q.add(2);
+// q.add(3);
 
-        System.out.print(q.remove() + " ");
-        q.add(4);
-        System.out.print(q.remove() + " ");
-        q.add(5);
+// System.out.print(q.remove() + " ");
+// q.add(4);
+// System.out.print(q.remove() + " ");
+// q.add(5);
 
-        while (!q.isEmpty()) {
-            System.out.print(q.peek() + " ");
-            q.remove();
-        }
+// while (!q.isEmpty()) {
+// System.out.print(q.peek() + " ");
+// q.remove();
+// }
 
-    }
-}
+// }
+// }
+
+// //****************************** queue implementation using linked list
+
+// public class Queues {
+// static class Node {
+// int data;
+// Node next;
+
+// Node(int data) {
+// this.data = data;
+// this.next = null;
+// }
+
+// }
+
+// static class queue {
+// static Node head = null;
+// static Node tail = null;
+
+// public static boolean isEmpty() {
+// return head == null && tail == null;
+// }
+
+// //add
+// public static void add(int data) {
+
+// Node newNode = new Node(data);
+// if (isEmpty()) {
+// head = tail = newNode;
+// } else {
+// tail.next = newNode;
+// tail = tail.next;
+// }
+
+// }
+
+// // remove
+// public static int remove() {
+// if (isEmpty()) {
+// System.out.println("Queue is empty");
+// return -1;
+// } else if (head == tail) {// if only one element
+// int val = head.data;
+// head = tail = null;
+// return val;
+
+// } else {
+// int val = head.data;
+// head = head.next;
+// return val;
+// }
+
+// }
+
+// public static int peek() {
+// if (isEmpty()) {
+// System.out.println("Queue is empty");
+// return -1;
+// }
+
+// return head.data;
+// }
+// }
+
+// public static void main(String[] args) {
+// queue q = new queue();
+// q.add(1);
+// q.add(2);
+// q.add(3);
+// q.add(4);
+// q.add(5);
+
+// while (!q.isEmpty()) {
+// System.out.print(q.peek() + " ");
+// q.remove();
+// }
+// }
+// }
+
+// //****************************** Queue in JCF
+
+// import java.util.*;
+
+// public class Queues {
+// public static void main(String[] args) {
+// // Queue<Integer> q = new LinkedList<>();
+// Queue<Integer> q = new ArrayDeque<>();
+
+// q.add(1);
+// q.add(2);
+// q.add(3);
+
+// while (!q.isEmpty()) {
+// System.out.print(q.peek() + " ");
+// q.remove();
+// }
+// }
+// }
+
+// //****************************** Queue using two stacks
+
+// // code 1 where add : O(n) , remove : O(1) , peek : O(1)
+// import java.util.*;
+
+// public class Queues {
+
+// static class queue {
+// static Stack<Integer> s1 = new Stack<>();
+// static Stack<Integer> s2 = new Stack<>();
+
+// public static boolean isEmpty() {
+// return s1.isEmpty();
+// }
+
+// // add : O(n)
+// public static void add(int data) {
+// while (!s1.isEmpty()) {
+// s2.push(s1.pop());
+// }
+
+// s1.push(data);
+
+// while (!s2.isEmpty()) {
+// s1.push(s2.pop());
+// }
+// }
+
+// // remove: O(1)
+// public static int remove() {
+// if (isEmpty()) {
+// System.out.println("Queue is empty");
+// return -1;
+// }
+
+// return s1.pop();
+// }
+
+// // peek : O(1)
+// public static int peek() {
+// if (isEmpty()) {
+// System.out.println("Queue is empty");
+// return -1;
+// }
+
+// return s1.peek();
+// }
+// }
+
+// public static void main(String[] args) {
+// queue q = new queue();
+// q.add(1);
+// q.add(2);
+// q.add(3);
+
+// while (!q.isEmpty()) {
+// System.out.print(q.peek() + " ");
+// q.remove();
+// }
+// }
+// }
+
+// //or
+
+// // code 2 where add : O(1) , remove : O(1) , peek : O(n)
+
+// import java.util.*;
+
+// public class Queues {
+
+// static class queue {
+// static Stack<Integer> s1 = new Stack<>();
+// static Stack<Integer> s2 = new Stack<>();
+
+// public static boolean isEmpty() {
+// return s1.isEmpty();
+// }
+
+// // add : O(1)
+// public static void add(int data) {
+// s1.push(data);
+// }
+
+// // remove: O(n)
+// public static int remove() {
+// if (isEmpty()) {
+// System.out.println("Queue is empty");
+// return -1;
+// }
+
+// while (!s1.isEmpty()) {
+// s2.push(s1.pop());
+// }
+
+// int val = s2.pop();
+
+// while (!s2.isEmpty()) {
+// s1.push(s2.pop());
+// }
+
+// return val;
+// }
+
+// // peek : O(1)
+// public static int peek() {
+// if (isEmpty()) {
+// System.out.println("Queue is empty");
+// return -1;
+// }
+
+// while (!s1.isEmpty()) {
+// s2.push(s1.pop());
+// }
+
+// int val = s2.peek();
+
+// while (!s2.isEmpty()) {
+// s1.push(s2.pop());
+// }
+
+// return val;
+// }
+// }
+
+// public static void main(String[] args) {
+// queue q = new queue();
+// q.add(1);
+// q.add(2);
+// q.add(3);
+
+// while (!q.isEmpty()) {
+// System.out.print(q.peek() + " ");
+// q.remove();
+// }
+// }
+// }
+
+// //****************************** Stack using two queues
+
+// // code 1 where push : O(n) , pop : O(1) , peek : O(1)
+
+// import java.util.*;
+
+// public class Queues {
+// static class stack {
+// static Queue<Integer> q1 = new LinkedList<>();
+// static Queue<Integer> q2 = new LinkedList<>();
+
+// public static boolean isEmpty() {
+// return q1.isEmpty();
+// }
+
+// public static void push(int data) {
+
+// q2.add(data);
+
+// while (!q1.isEmpty()) {
+// q2.add(q1.remove());
+// }
+
+// while (!q2.isEmpty()) {
+// q1.add(q2.remove());
+// }
+// }
+
+// public static int pop() {
+// if (isEmpty()) {
+// System.out.println("stack is empty");
+// return -1;
+// }
+
+// return q1.remove();
+// }
+
+// public static int peek() {
+// if (isEmpty()) {
+// System.out.println("stack is empty");
+// return -1;
+// }
+
+// return q1.peek();
+// }
+// }
+
+// public static void main(String[] args) {
+// Stack<Integer> s = new Stack<>();
+// s.push(1);
+// s.push(2);
+// s.push(3);
+
+// while (!s.isEmpty()) {
+// System.out.print(s.peek() + " ");
+// s.pop();
+// }
+// }
+// }
+
+// //or
+// // code 2 where push : O(1) , pop : O(n) , peek : O(n)
+
+// import java.util.*;
+
+// public class Queues {
+// static class stack {
+// static Queue<Integer> q1 = new LinkedList<>();
+// static Queue<Integer> q2 = new LinkedList<>();
+
+// public static boolean isEmpty() {
+// return q1.isEmpty() && q2.isEmpty();
+// }
+
+// public static void push(int data) {
+// if (!q1.isEmpty()) {
+// q1.add(data);
+// } else {
+// q2.add(data);
+// }
+// }
+
+// public static int pop() {
+// if (isEmpty()) {
+// System.out.println("stack is empty");
+// return -1;
+// }
+
+// int top = -1;
+
+// if (!q1.isEmpty()) {
+// while (!q1.isEmpty()) {
+// top = q1.remove();
+// if (q1.isEmpty()) {
+// break;
+// }
+// q2.add(top);
+// }
+// } else {
+// while (!q2.isEmpty()) {
+// top = q2.remove();
+// if (q2.isEmpty()) {
+// break;
+// }
+// q1.add(top);
+// }
+// }
+
+// return top;
+// }
+
+// public static int peek() {
+// if (isEmpty()) {
+// System.out.println("stack is empty");
+// return -1;
+// }
+
+// int top = -1;
+
+// if (!q1.isEmpty()) {
+// while (!q1.isEmpty()) {
+// top = q1.remove();
+// q2.add(top);
+// }
+// } else {
+// while (!q2.isEmpty()) {
+// top = q2.remove();
+// q1.add(top);
+// }
+// }
+
+// return top;
+// }
+// }
+
+// public static void main(String[] args) {
+// Stack<Integer> s = new Stack<>();
+// s.push(1);
+// s.push(2);
+// s.push(3);
+
+// while (!s.isEmpty()) {
+// System.out.print(s.peek() + " ");
+// s.pop();
+// }
+// }
+// }
+
+// //***************** First non-repeating character in a stream of characters
+
+// import java.util.*;
+
+// public class Queues {
+
+// public static void printNonReprating(String str) {
+// int[] freq = new int[26];
+// Queue<Character> q = new LinkedList<>();
+
+// for (int i = 0; i < str.length(); i++) {
+// char ch = str.charAt(i);
+// q.add(ch);
+// freq[ch - 'a']++;
+
+// while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+// q.remove();
+// }
+
+// if (q.isEmpty()) {
+// System.out.print(-1 + " ");
+// } else {
+// System.out.print(q.peek() + " ");
+// }
+// }
+// }
+
+// public static void main(String[] args) {
+// String str = "aabccxb";
+// printNonReprating(str);
+
+// }
+// }
+
+// //****************************** Interleave 2 halves of a queue(even length)
+
+// import java.util.*;
+
+// public class Queues {
+
+// public static void interLeave(Queue<Integer> q) {
+// int size = q.size();
+
+// Queue<Integer> firstHalf = new LinkedList<>();
+
+// for (int i = 0; i < size / 2; i++) {
+// firstHalf.add(q.remove());
+// }
+
+// while (!firstHalf.isEmpty()) {
+// q.add(firstHalf.remove());
+// q.add(q.remove());
+// }
+// }
+
+// public static void main(String[] args) {
+// Queue<Integer> q = new LinkedList<>();
+// q.add(1);
+// q.add(2);
+// q.add(3);
+// q.add(4);
+// q.add(5);
+// q.add(6);
+// q.add(7);
+// q.add(8);
+// q.add(9);
+// q.add(10);
+
+// interLeave(q);
+
+// while (!q.isEmpty()) {
+// System.out.print(q.peek() + " ");
+// q.remove();
+// }
+// }
+// }
+
+// //****************************** queue reversal
+
+// import java.util.*;
+
+// public class Queues {
+
+// public static void reverse(Queue<Integer> q) {
+// Stack<Integer> s = new Stack<>();
+
+// while (!q.isEmpty()) {
+// s.push(q.remove());
+// }
+
+// while (!s.isEmpty()) {
+// q.add(s.pop());
+// }
+// }
+
+// public static void main(String[] args) {
+// Queue<Integer> q = new LinkedList<>();
+// q.add(1);
+// q.add(2);
+// q.add(3);
+// q.add(4);
+// q.add(5);
+
+// reverse(q);
+
+// while (!q.isEmpty()) {
+// System.out.print(q.remove() + " ");
+// }
+
+// System.out.println();
+// }
+// }
+
+// //****************************** Deque implementation
+
+// import java.util.*;
+
+// public class Queues {
+// public static void main(String[] args) {
+// Deque<Integer> deque = new LinkedList<>();
+
+// deque.addFirst(1);
+// deque.addFirst(2);
+// deque.addLast(3);
+// deque.addLast(4);
+
+// System.out.println(deque);
+
+// deque.removeFirst();
+// deque.removeLast();
+// System.out.println(deque);
+
+// System.out.println("first element: " + deque.getFirst());
+// System.out.println("last element: " + deque.getLast());
+// }
+// }
+
+// //****************************** Stack creation using Deque
+
+// import java.util.*;
+
+// public class Queues {
+
+// static class Stack {
+// Deque<Integer> deque = new LinkedList<>();
+
+// public void push(int data) {
+// deque.addLast(data);
+// }
+
+// public int pop() {
+// return deque.removeLast();
+// }
+
+// public int peek() {
+// return deque.getLast();
+// }
+
+// }
+
+// public static void main(String[] args) {
+// Stack s = new Stack();
+// s.push(1);
+// s.push(2);
+// s.push(3);
+
+// // Note : ismai is Empty function use mat karna kyuki woh banaya nhi hai
+// deque
+// // se
+
+// System.out.println("peek = " + s.peek());
+// System.out.println(s.pop());
+// System.out.println(s.pop());
+// System.out.println(s.pop());
+
+// }
+// }
+
+// //****************************** Queue creation using Deque
+
+// import java.util.*;
+
+// public class Queues {
+
+// static class queue {
+// Deque<Integer> deque = new LinkedList<>();
+
+// public void add(int data) {
+// deque.addLast(data);
+// }
+
+// public int remove() {
+// return deque.removeFirst();
+// }
+
+// public int peek() {
+// return deque.getFirst();
+// }
+
+// }
+
+// public static void main(String[] args) {
+// queue q = new queue();
+// q.add(1);
+// q.add(2);
+// q.add(3);
+
+// // Note : ismai is Empty function use mat karna kyuki woh banaya nhi hai
+// deque se
+
+// System.out.println("peek = " + q.peek());
+// System.out.println(q.remove());
+// System.out.println(q.remove());
+// System.out.println(q.remove());
+
+// }
+// }
--- DSA/DSA/3.)Linear data structures/3.)Stacks and Queues/Queues/practiceSet.java
+++ DSA/DSA/3.)Linear data structures/3.)Stacks and Queues/Queues/practiceSet.java
@@ -1,3 +1,223 @@
+// Note : assignment question 2,3 mai priority queue use hoga toh usse heaps
+// chapter ke baad karna hai abhi nhi jamega
+
+// //****************************** practice question 1
+
+// import java.util.*;
+
+// public class practiceSet {
+
+//     public static void printBinary(int n) {
+//         Queue<String> q = new LinkedList<>();
+//         q.add("1");
+
+//         while (n-- > 0) {
+//             String s1 = q.remove();
+
+//             System.out.println(s1);
+
+//             q.add(s1 + "0");
+//             q.add(s1 + "1");
+//         }
+
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 10;
+//         printBinary(n);
+
+//     }
+// }
+
+// //****************************** practice question 4
+
+// import java.util.*;
+
+// public class practiceSet {
+
+//     public static void reverseQueueFirstKElements(Queue<Integer> q, int k) {
+//         Stack<Integer> s = new Stack<>();
+
+//         if (q.isEmpty() == true || k > q.size()) {
+//             return;
+//         }
+//         if (k <= 0) {
+//             return;
+//         }
+
+//         // push first k elements into stack
+//         for (int i = 0; i < k; i++) {
+//             s.push(q.remove());
+//         }
+
+//         // enqueu the stack elements back to queue
+//         while (!s.isEmpty()) {
+//             q.add(s.pop());
+//         }
+
+//         // remove the remaining elements and enqueue them back
+//         for (int i = 0; i < q.size() - k; i++) {
+//             q.add(q.remove());
+//         }
+
+//     }
+
+//     public static void main(String[] args) {
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+//         q.add(6);
+//         q.add(7);
+//         q.add(8);
+//         q.add(9);
+//         q.add(10);
+
+//         int k = 5;
+
+//         reverseQueueFirstKElements(q, k);
+
+//         while (!q.isEmpty()) {
+//             System.out.print(q.remove() + " ");
+//         }
+//     }
+// }
+
+// // ***************************or 
+
+// import java.util.*;
+
+// public class practiceSet {
+
+//     public static void helper(Queue<Integer> q, int k) {
+//         // base case
+//         if (k == 0) {
+//             return;
+//         }
+
+//         int temp = q.remove();
+//         helper(q, k - 1);
+//         q.add(temp);
+//     }
+
+//     public static void reverseQueueFirstKElements(Queue<Integer> q, int k) {
+
+//         if (q.isEmpty() == true || k > q.size()) {
+//             return;
+//         }
+//         if (k <= 0) {
+//             return;
+//         }
+
+//         helper(q, k);
+
+//         // remove the remaining elements and enqueue them back
+//         for (int i = 0; i < q.size() - k; i++) {
+//             q.add(q.remove());
+//         }
+
+//     }
+
+//     public static void main(String[] args) {
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(1);
+//         q.add(2);
+//         q.add(3);
+//         q.add(4);
+//         q.add(5);
+//         q.add(6);
+//         q.add(7);
+//         q.add(8);
+//         q.add(9);
+//         q.add(10);
+
+//         int k = 5;
+
+//         reverseQueueFirstKElements(q, k);
+
+//         while (!q.isEmpty()) {
+//             System.out.print(q.remove() + " ");
+//         }
+//     }
+// }
+
+// //****************************** practice question 5
+
+// // brute force approach
+// public class practiceSet {
+
+//     public static void printMax(int[] arr, int n, int k) {
+//         for (int i = 0; i < n - k + 1; i++) {
+//             int max = arr[i];
+//             for (int j = i; j < i + k; j++) {
+//                 if (arr[j] > max) {
+//                     max = arr[j];
+//                 }
+//             }
+//             System.out.print(max + " ");
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = { 1, 2, 3, 1, 4, 5, 2, 3, 6 };
+//         int k = 3;
+//         printMax(arr, arr.length, k);
+//     }
+// }
+
+// // optimised approach
+
+import java.util.*;
+
 public class practiceSet {
 
+    public static void printMax(int[] arr, int n, int k) {
+
+        Deque<Integer> dq = new LinkedList<>();
+
+        for (int i = 0; i < k; i++) {
+
+            // remove all the smaller elements from back
+            while (!dq.isEmpty() && arr[dq.getLast()] <= arr[i]) {
+                dq.removeLast();
+            }
+
+            // add current element at back of deque
+            dq.addLast(i);
+        }
+
+        // print first window max
+        System.out.print(arr[dq.peek()] + " ");
+
+        // process rest of the elements
+        // i.e., from arr[k] to arr[n-1]
+
+        for (int i = k; i < n; i++) {
+
+            // reomove elements which are out of this window
+            while (!dq.isEmpty() && dq.peek() <= i - k) {
+                dq.removeFirst();
+            }
+
+            // remove all the smaller elements from back
+            while (!dq.isEmpty() && arr[dq.getLast()] <= arr[i]) {
+                dq.removeLast();
+            }
+
+            // add current element at back of deque
+            dq.addLast(i);
+
+            // print the maximum element of current window
+            System.out.print(arr[dq.peek()] + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 1, 4, 5, 2, 3, 6 };
+        int k = 3;
+        printMax(arr, arr.length, k);
+    }
 }
